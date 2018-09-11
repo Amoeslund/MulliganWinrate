41f798a5e4da89c4293ed4d7bcec24f6c17d9300
AwesomeAL <alleneustace@gmail.com>
2018-09-11 11:41:31 +0200

    Moved json to json class file

--- src/MulliganWinrate.cs
+++ src/MulliganWinrate.cs
@@ -187,45 +187,5 @@ namespace MulliganWinrate
         }
     }
 
-    public class Metadata
-    {
-        public string earliest_date { get; set; }
-        public string latest_date { get; set; }
-        public int total_games { get; set; }
-        public double base_winrate { get; set; }
-    }
 
-    public class ALL
-    {
-        public int dbf_id { get; set; }
-        public int times_presented_in_initial_cards { get; set; }
-        public int times_kept { get; set; }
-        public double keep_percentage { get; set; }
-        public int times_in_opening_hand { get; set; }
-        public double opening_hand_winrate { get; set; }
-        public int times_card_drawn { get; set; }
-        public double winrate_when_drawn { get; set; }
-        public int times_card_played { get; set; }
-        public double avg_turn_played_on { get; set; }
-        public double avg_turns_in_hand { get; set; }
-        public double winrate_when_played { get; set; }
-    }
-
-    public class Data
-    {
-        public List<ALL> ALL { get; set; }
-    }
-
-    public class Series
-    {
-        public Metadata metadata { get; set; }
-        public Data data { get; set; }
-    }
-
-    public class RootObject
-    {
-        public string render_as { get; set; }
-        public Series series { get; set; }
-        public DateTime as_of { get; set; }
-    }
 }
